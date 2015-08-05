912006f21f705626b2811c62754f67b521b19e94
Kami Barut-Wanayo <kamibarut@yahoo.com>
2015-08-05 20:12:00 +0200

    Create about.php

---
+++ routing-project/src/about.php
@@ -0,0 +1,19 @@
+<?php 
+
+	class About	{
+			
+		public function__construct()	{
+			
+			echo "This is the about page.";
+			$this->_other();
+			
+		}
+			
+		public function  _other()	{
+			
+			echo "Some other function.";
+			
+		}
+	}
+
+?>
