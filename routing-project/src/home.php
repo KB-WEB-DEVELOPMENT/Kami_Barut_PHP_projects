f6293add9640ff4517000289951d5417a5a0622d
Kami Barut-Wanayo <kamibarut@yahoo.com>
2015-08-05 20:11:37 +0200

    Create home.php

---
+++ routing-project/src/home.php
@@ -0,0 +1,18 @@
+<?php 
+	
+	class Home	{
+			
+		public function__construct()	{
+			
+			echo "This is the home page.";
+			$this->_other();
+			
+		}
+			
+		public function  _other()	{
+			
+			echo "Some other function.";
+			
+		}
+	}
+?>
