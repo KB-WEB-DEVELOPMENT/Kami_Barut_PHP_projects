e04c18d789cbbc690e38e042e3febfca388d452c
Kami Barut-Wanayo <kamibarut@yahoo.com>
2015-08-05 20:12:37 +0200

    Create contact.php

---
+++ routing-project/src/contact.php
@@ -0,0 +1,16 @@
+<?php 
+	
+	class Contact	{
+			
+		public function__construct()	{
+			
+			echo "This is the contact page.";
+			$this->_other();
+		}
+			
+		public function  _other()	{
+			
+			echo "Some other function.";
+		}
+	}
+?>
