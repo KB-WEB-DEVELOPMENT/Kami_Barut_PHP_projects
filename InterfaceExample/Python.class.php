ff35578c23a9cb4aee22b6943a380f7c7afb8939
Kami Barut-Wanayo <kamibarut@yahoo.com>
2015-08-06 10:09:03 +0200

    Create Python.class.php

---
+++ InterfaceExample/Python.class.php
@@ -0,0 +1,18 @@
+<?php
+
+class Python	{
+		
+  	public function __construct() {
+  		
+  		echo "Python was created ";
+  		$this->UsesDotSyntax()
+  	}
+  		
+  	public function UsesDotSyntax() {
+  		
+  		echo "and the method UsesDotSyntax is called from within the Python class.";
+  		
+  	}		
+}
+	
+?>
