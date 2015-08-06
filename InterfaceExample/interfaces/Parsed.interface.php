6c541dbc942ac5c9ea2fb801d887d57068e4b9e5
Kami Barut-Wanayo <kamibarut@yahoo.com>
2015-08-06 10:02:17 +0200

    Create Parsed.interface.php

---
+++ InterfaceExample/interfaces/Parsed.interface.php
@@ -0,0 +1,11 @@
+<?php
+
+	interface Parsed	{
+	
+  	   public function parsed($someOptionalVariable)	{
+  	
+  		  echo "I am the function parsed() from the interface Parsed echoing this text"; 
+  	   }
+	
+	}
+?>
