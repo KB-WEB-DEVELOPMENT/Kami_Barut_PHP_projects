227eb39a2307987e17ec32383a5b4ebcc3d4b4ae
Kami Barut-Wanayo <kamibarut@yahoo.com>
2015-08-06 10:03:29 +0200

    Create DotSyntax.interface.php

---
+++ InterfaceExample/interfaces/DotSyntax.interface.php
@@ -0,0 +1,12 @@
+<?php
+
+	interface DotSyntax	{
+		
+			public function UsesDotSyntax(){
+		
+			  echo "method UsesDotSyntax is called from within the DotSyntax interface.";
+		
+		  }
+		
+	}
+?>
