6613875eaad3603078781915409f50d4476f043b
Kami Barut-Wanayo <kamibarut@yahoo.com>
2015-08-06 10:06:54 +0200

    Create PHP.class.php

---
+++ InterfaceExample/PHP.class.php
@@ -0,0 +1,10 @@
+<?php
+
+		class PHP implements Parsed	{
+					
+			public function parsed($someOptionalVariable)	{
+		
+				echo "I am the function parsed in the PHP class coming from the Parsed interface"; 
+			}	
+		}
+?>
