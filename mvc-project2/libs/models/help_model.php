ff52857d87a7c3322b84b79deb8a669a63bc3adc
Kami Barut-Wanayo <kamibarut@yahoo.com>
2015-08-20 15:48:12 +0200

    Create help_model.php

---
+++ mvc-project2/libs/models/help_model.php
@@ -0,0 +1,15 @@
+<?php
+
+class Help_Model extends Model {
+
+    public function __construct() {
+        parent::__construct();
+    }
+    
+    public function blah() {
+        return "anything, just a function";
+    }
+
+}
+
+?>
