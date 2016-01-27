699499da8d06dc5f6786ffb728677c2c94ea730a
Kami Barut-Wanayo <kamibarut@yahoo.com>
2016-01-27 21:43:08 +0100

    Create Rest.php

---
+++ REST-API-Web-Service/Rest.php
@@ -0,0 +1,50 @@
+<?php
+
+	class Rest {
+
+		function curl_get_call($url) {
+
+			$headers = array('Accept:application/json','Content-Type:application/json');
+
+			$handle = curl_init();
+
+			curl_setopt($handle, CURLOPT_URL, $url);
+			curl_setopt($handle, CURLOPT_HTTPHEADER, $headers);
+			curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($handle, CURLOPT_SSL_VERIFYHOST, false);
+			curl_setopt($handle, CURLOPT_SSL_VERIFYPEER, false);
+
+			$reponse = curl_exec($handle);
+			
+			$code = curl_getinfo($handle, CURLINFO_HTTP_CODE);
+
+			return $reponse; 
+
+		}
+
+
+		function curl_post_call($url, $data) {
+
+			$headers = array('Accept:application/json','Content-Type:application/json');
+
+			$handle = curl_init();
+
+			curl_setopt($handle, CURLOPT_URL, $url);
+			curl_setopt($handle, CURLOPT_HTTPHEADER, $headers);
+			curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($handle, CURLOPT_SSL_VERIFYHOST, false);
+			curl_setopt($handle, CURLOPT_SSL_VERIFYPEER, false);
+			curl_setopt($handle, CURLOPT_POST, true);
+			curl_setopt($handle, CURLOPT_POSTFIELDS, $data);
+
+			$reponse = curl_exec($handle);
+			
+			$code = curl_getinfo($handle, CURLINFO_HTTP_CODE);
+
+			return $reponse; 
+
+		}
+
+	}
+
+?>
