bbe420745d9e50ce20641266986e8d0ad593ccf2
Kami Barut-Wanayo <kamibarut@yahoo.com>
2016-01-27 22:23:31 +0100

    Update add_comment.php

--- REST-API-Web-Service/add_comment.php
+++ REST-API-Web-Service/add_comment.php
@@ -8,12 +8,12 @@ $moviename = $_GET["moviename"];
 			"<html>" 	.
 			
 				"<head>"	.
-					'<script type=\"text/javascript"' . 'src=\"js/query-1.7.1min.js"></script>' .
-					'<script type=\"text/javascript"' . 'src=\"js/query-ui-1.8.17.custom.min.js"></script>'	.
-					'<script type=\"text/javascript"' . 'src=\"js/js.js"></script>'	.
-					'<script type=\"text/javascript"' . 'src=\"js/modal.js"></script>'	.
-					'<link rel=\"stylesheet"' . 'type="text/css"' . 'href=\"css/style.css"/>' 	.
-					'<link rel=\"stylesheet"' . 'type="text/css"' . 'href=\"css/jquery-ui-1.8.17.custom.css" />'	. 
+					'<script type="text/javascript"' . 'src="js/query-1.7.1min.js"></script>' .
+					'<script type="text/javascript"' . 'src="js/query-ui-1.8.17.custom.min.js"></script>'	.
+					'<script type="text/javascript"' . 'src="js/js.js"></script>'	.
+					'<script type="text/javascript"' . 'src="js/modal.js"></script>'	.
+					'<link rel="stylesheet"' . 'type="text/css"' . 'href="css/style.css"/>' 	.
+					'<link rel="stylesheet"' . 'type="text/css"' . 'href="css/jquery-ui-1.8.17.custom.css" />'	. 
 				"</head>"	.
 
 				"<body>"	.	
@@ -23,8 +23,8 @@ $moviename = $_GET["moviename"];
 					'<form id="commentform" action=""  methood="post">'	.
 						'Comment:<br><textarea cols="25" name="comment"></textarea><br>'	.
 						'Name:<br><input type="text" name="name" /><br>'	.
-						'input type="submit" name="submit" value="Submit" onclick=\"submitcomment(); return false; "/>'	.	
-						'input type="submit" name="submit" value="Cancel" onclick=\"closedialog(); return false; "/>'	.
+						'input type="submit" name="submit" value="Submit" onclick="submitcomment(); return false; "/>'	.	
+						'input type="submit" name="submit" value="Cancel" onclick="closedialog(); return false; "/>'	.
 					'</form>'	.
 				"</body>"	.
 			"</html>" 			
