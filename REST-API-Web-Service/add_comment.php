017cd2adaa52d53af3b23a79a28dff3b33dbce4d
Kami Barut-Wanayo <kamibarut@yahoo.com>
2016-01-27 21:39:33 +0100

    Create add_comment.php

---
+++ REST-API-Web-Service/add_comment.php
@@ -0,0 +1,31 @@
+<?php
+
+$movieid = $_GET["id"];
+$moviename = $_GET["moviename"];
+
+
+	echo "<!DOCTYPE html>" .
+			"<html>" 	.
+			
+				"<head>"	.
+					'<script type=\"text/javascript"' . 'src=\"js/query-1.7.1min.js"></script>' .
+					'<script type=\"text/javascript"' . 'src=\"js/query-ui-1.8.17.custom.min.js"></script>'	.
+					'<script type=\"text/javascript"' . 'src=\"js/js.js"></script>'	.
+					'<script type=\"text/javascript"' . 'src=\"js/modal.js"></script>'	.
+					'<link rel=\"stylesheet"' . 'type="text/css"' . 'href=\"css/style.css"/>' 	.
+					'<link rel=\"stylesheet"' . 'type="text/css"' . 'href=\"css/jquery-ui-1.8.17.custom.css" />'	. 
+				"</head>"	.
+
+				"<body>"	.	
+
+					"Movie title:<b>"  $moviename . "</b>"	.
+
+					'<form id="commentform" action=""  methood="post">'	.
+						'Comment:<br><textarea cols="25" name="comment"></textarea><br>'	.
+						'Name:<br><input type="text" name="name" /><br>'	.
+						'input type="submit" name="submit" value="Submit" onclick=\"submitcomment(); return false; "/>'	.	
+						'input type="submit" name="submit" value="Cancel" onclick=\"closedialog(); return false; "/>'	.
+					'</form>'	.
+				"</body>"	.
+			"</html>" 			
+?>
